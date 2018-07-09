f5a0301ca2e915592d09b8d3f2199dc6de07948d
watasi <jessoncheung@gmail.com>
2018-07-09 16:46:36 +0800

    修改文章编辑页面，重新配置markdown编辑器

--- src/components/VEditor.vue
+++ src/components/VEditor.vue
@@ -3,9 +3,11 @@
 		<mavon-editor
 			style='height:100%;'
 			:value='value'
-			:subfield='false'
+      ref='md'
+      @imgAdd="$imgAdd"
 			:boxShadow='false'
-			@change='onchange'>
+      :toolbars='toolbars'
+			@change='getMd'>
 		</mavon-editor>
 	</div>
 </template>
@@ -14,14 +16,68 @@
 import { mavonEditor } from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
 export default {
-	name: 'VEditor',
+  name: 'VEditor',
+  data () {
+    return {
+      toolbars: {
+        bold: true, // 粗体
+        italic: true, // 斜体
+        header: true, // 标题
+        underline: true, // 下划线
+        strikethrough: true, // 中划线
+        mark: true, // 标记
+        superscript: true, // 上角标
+        subscript: true, // 下角标
+        quote: true, // 引用
+        ol: true, // 有序列表
+        ul: true, // 无序列表
+        link: true, // 链接
+        imagelink: true, // 图片链接
+        code: true, // code
+        table: true, // 表格
+        fullscreen: false, // 全屏编辑
+        readmodel: false, // 沉浸式阅读
+        htmlcode: false, // 展示html源码
+        help: true, // 帮助
+        /* 1.3.5 */
+        undo: true, // 上一步
+        redo: true, // 下一步
+        trash: true, // 清空
+        save: false, // 保存（触发events中的save事件）
+        /* 1.4.2 */
+        navigation: true, // 导航目录
+        /* 2.1.8 */
+        alignleft: true, // 左对齐
+        aligncenter: true, // 居中
+        alignright: true, // 右对齐
+        /* 2.2.1 */
+        subfield: true, // 单双栏模式
+        // 预览
+        preview: true
+      }
+    }
+  },
 	props: {
 		value: String
 	},
 	methods: {
-		onchange(val, render) {
-			this.$emit('change', val, render)
-		}
+		getMd(val, render) {
+			return { val, render }
+    },
+    // 上传图片
+    $imgAdd(pos, $file) {
+      console.log(pos, $file)
+      /* var formdata = new FormData()
+      formdata.append('image', $file)
+      this.$axios({
+        url: 'server url',
+        method: 'post',
+        data: formdata,
+        headers: { 'Content-Type': 'multipart/form-data' }
+      }).then(url => {
+        this.$refs.md.$img2Url(pos, url)
+      }) */
+    }
 	},
 	components: {
 		mavonEditor
--- src/pages/Editor.vue
+++ src/pages/Editor.vue
@@ -3,11 +3,11 @@
     <el-row>
       <el-col>
         <slide-com title='文章'>
-          <el-form :model="editorForm" :rules="rules" ref="ruleForm">
+          <el-form :model="editorForm" :rules="rules" ref="editorForm">
             <!-- 标题 -->
             <el-form-item prop='title'>
               <el-input placeholder="请输入标题" v-model="editorForm.title">
-                <el-select v-model="select" slot="prepend">
+                <el-select v-model="editorForm.type" slot="prepend">
                   <el-option label="原创" value="1"></el-option>
                   <el-option label="转载" value="2"></el-option>
                   <el-option label="翻译" value="3"></el-option>
@@ -15,7 +15,7 @@
               </el-input>
             </el-form-item>
             <!-- 原文地址 -->
-            <el-form-item v-show='select!=1' prop='originalText'>
+            <el-form-item v-show='editorForm.type!=1' prop='originalText'>
               <el-input v-model='editorForm.originalText' placeholder='原文地址'></el-input>
             </el-form-item>
             <!-- 标签 -->
@@ -23,19 +23,19 @@
               <el-input v-model='editorForm.tag' placeholder='标签，如：vue 可使用逗号,分好;分割'></el-input>
             </el-form-item>
             <!-- 内容 -->
-            <el-form-item>
-              <v-editor />
+            <el-form-item prop='content'>
+              <v-editor :value='editorForm.content' />
             </el-form-item>
             <!-- 置顶 && 推荐 -->
             <el-form-item>
-              <el-checkbox-group v-model="editorForm.type">
-                <el-checkbox label="置顶" name="type"></el-checkbox>
-                <el-checkbox label="推荐" name="type"></el-checkbox>
+              <el-checkbox-group v-model="editorForm.hand">
+                <el-checkbox label="置顶" value='0' name="type"></el-checkbox>
+                <el-checkbox label="推荐" value='1' name="type"></el-checkbox>
               </el-checkbox-group>
             </el-form-item>
             <!-- 发布 -->
             <el-form-item>
-              <el-button size='small' type='primary'>发布文章</el-button>
+              <el-button size='small' @click='submit("editorForm")' type='primary'>发布文章</el-button>
             </el-form-item>
           </el-form>
         </slide-com>
@@ -49,14 +49,28 @@ import VEditor from '@/components/VEditor.vue'
 export default {
   data () {
     return {
-      select: '1',
       editorForm: {
         title: '',
-        type: [],
+        hand: [],
         tag: '',
-        originalText: ''
+        type: '1',
+        originalText: '',
+        content: ''
       },
-      rules: {}
+      rules: {
+        title: [ { required: true, message: '请输入标题', trigger: 'blur' } ]
+      }
+    }
+  },
+  methods: {
+    submit(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$axios.post('/v1/admin', this.editorForm)
+        } else {
+          return false
+        }
+      })
     }
   },
   components: {
